d604e326d350927ec5b6f3f97118f136f1b54b58
Millicent Omwenga <tichi52@gmail.com>
2022-12-12 14:11:26 -0800

    Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * to find size of int 
+ * float
+ * double
+ * character
+ *
+ * main - Entry point
+ *
+ * Return: always  0
+ */
+
+int main(void)
+{
+	printf("size of float\n");
+	printf("size of char\n");
+	printf("size of double\n");
+	printf("size of int\n");
+
+	return (0);
+}
