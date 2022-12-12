f62d1665053aea5907a959abf9c254e36513336e
Millicent Omwenga <tichi52@gmail.com>
2022-12-12 12:25:30 -0800

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include<stdio.h>
+/* *
+ * main -entry point
+ * return always 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return 0;
+}
