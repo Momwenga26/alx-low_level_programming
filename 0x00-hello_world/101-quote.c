ba420d5ca907153caf1c0fa8fd264b529f4be9bb
Millicent Omwenga <tichi52@gmail.com>
2022-12-13 01:21:18 -0800

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,11 +5,13 @@
  * main: Entry point
  *
  * Return: Always 1 (success)
+ *
+ * how to include quotation marks
  */
 
 int main(void)
 {
 	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
-	return(1);
+	return (1);
 }
