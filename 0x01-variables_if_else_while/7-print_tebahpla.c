c60c8caeebb2c321204b8faf57fd204382c55b96
Millicent Omwenga <tichi52@gmail.com>
2022-12-14 02:29:14 -0800

    Task 8

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,11 +3,10 @@
 #include <time.h>
 
 /**
- * main - Entry point Returns alphabet in reverse order
+ * main - Entry point prints lower case alphabets in reverse order
  *
  * Returns: Always 0 (success)
  *
- *returns alphabets in reverse order
  */
 
 int main(void)
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main -Entry point
+ *
+ * Returns: Always 0 (success)
+ *
+ * prints all numbers of base 16 in lowercase
+ */
+
+int main(void)
+{
+	int num;
+	char ch;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+	for (ch = 'a'; ch <= 'f'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
