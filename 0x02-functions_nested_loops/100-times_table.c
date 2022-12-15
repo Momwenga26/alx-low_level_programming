b0496a7b52e13da447673adacc30d34a1cf7a69a
Millicent Omwenga <tichi52@gmail.com>
2022-12-15 05:31:04 -0800

    Task 12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * print_times_table - prints times table
+ * @n: the number to be treated
+ * Return: number matrix
+ */
+void print_times_table(int n)
+{
+	int a, b, m;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (a = o; a <= n; a++)
+		{
+			_putchar(48);
+			for (b = 1; b <= n; b++)
+			{
+				m = a * b;
+				_putchar(44);
+				_putchar(32);
+				if (m <= 9)
+				{
+					_putchar(32);
+					_putchar(32);
+					_putchar(m + 48);
+				}
+				else if (m <= 99)
+				{
+					_putchar(32);
+					_putchar((m / 10) + 48);
+					_putchar((m % 10) + 48);
+				}
+				else
+				{
+					_putchar(((m / 100) % 10) + 48);
+					_putchar(((m / 10) % 10) + 48);
+					_putchar((m % 10) + 48);
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
