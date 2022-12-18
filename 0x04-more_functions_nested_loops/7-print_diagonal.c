dd36275e5c685a3f1570bb8e8500208ceeaf3fb4
Millicent Omwenga <tichi52@gmail.com>
2022-12-18 13:10:07 -0800

    Task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,19 +9,22 @@ void print_diagonal(int n)
 {
 	int i, j;
 
-	if (n > 0)
+	if (n <= 0)
+		_putchar('\n');
+
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
+		for (j = 0; j < n; j++)
 		{
-			for (j = 0; j < n; j++)
+			if (i == j)
+			{
+				_putchar('\\');
 				_putchar(' ');
-			_putchar('\\');
-
-			if (i == n - 1)
-				continue;
-
-			_putchar('\n');
+			}
+			else
+			{
+				_putchar(' ');
+			}
 		}
 	}
-	_putchar('\n');
 }
