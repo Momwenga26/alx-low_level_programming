537eef271854c47673fa3090def2a431ae6737ea
Millicent Omwenga <tichi52@gmail.com>
2022-12-18 03:40:04 -0800

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#iclude "main.h"
+
+/**
+ * _isupper - function that checks for uppercase characters
+ * @c: character to check
+ * Return: 1 if c is uppercase 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
