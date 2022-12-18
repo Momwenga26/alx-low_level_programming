459c804fffc3f5f2928e89bf78d71b1c61bb20e2
Millicent Omwenga <tichi52@gmail.com>
2022-12-18 10:27:13 -0800

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that prints the numbers, from 0 to 9
+ * followed by a new line
+ *
+ * Return: always 0 (success)
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0, i <= 9, i++)
+		_putchar(i + '0');
+
+	_putchar('\n');
+}
