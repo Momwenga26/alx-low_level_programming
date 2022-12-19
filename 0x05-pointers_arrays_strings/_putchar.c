c81d31d320b5aac88ad44828759a4114a57d6eab
Millicent Omwenga <tichi52@gmail.com>
2022-12-19 05:28:15 -0800

    Task 0

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to standout
+ * @c: the character to print
+ * Return: on success 1
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
