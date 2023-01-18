dde65ba3e01c18e6bc2e57dbe8c632bc76ece842
Millicent Omwenga <tichi52@gmail.com>
2023-01-18 03:35:33 -0800

    Task 5

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+
+int _strlen(char *str);
+char *_strcopy(char *dest, char *src);
+dog_t *new_dog(char *name, float age, char *owner);
+
+/**
+ * _strlen - finds the length of a string
+ * @str: the string to be measured
+ *
+ * Return: the length of the string
+ */
+
+int _strlen(char *str)
+
