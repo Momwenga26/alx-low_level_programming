777194bd98f624688029c6289305d227fc948d12
Millicent Omwenga <tichi52@gmail.com>
2023-01-31 22:09:55 -0800

    Task 0

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _putchar - prints the character c to stdout
+ * @c: the character to print
+ *
+ * Return: On success 1
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
