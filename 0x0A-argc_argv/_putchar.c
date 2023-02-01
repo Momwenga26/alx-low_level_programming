2defe1c2df4cb461bd759d2f3ea6db56ee3850f9
Millicent Omwenga <tichi52@gmail.com>
2023-02-01 03:13:28 -0800

    Task 0

---
+++ 0x0A-argc_argv/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _putchar - prints the char c to stdout
+ * @c: the char to print
+ *
+ * Return: on success 1
+ */
+
+int _putchar(char c)
+{
+	return (write (1, &c, 1));
+}
